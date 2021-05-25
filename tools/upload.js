11e001ba725355a7260638888ebde13eb5c14c23
uctoo-liut <2394630102@qq.com>
2021-05-25 16:56:15 +0800

    update

--- tools/upload.js
+++ tools/upload.js
@@ -1,38 +1,38 @@
-const multer = require('multer')
-const fs = require('fs')
+const multer = require("multer");
+const fs = require("fs");
 const fileReName = function (sourceFile, destPath) {
   fs.rename(sourceFile, destPath, function (err) {
-    if (err) throw err
-    console.info({ sourceFile, destPath })
-  })
-}
+    if (err) throw err;
+    console.info({ sourceFile, destPath });
+  });
+};
 // 上传文件中间件
 const middlewares = {
   // 单文件上传
   upload: (req, res, next) => {
     // upload 目录下存储上传图片
-    let upload = multer({ dest: 'upload/' }).single('photo')
+    let upload = multer({ dest: "upload/" }).single("photo");
     upload(req, res, (err) => {
       // console.info(req.file);
       if (err) {
         res.json({
           data: null,
-          meta: { msg: '上传文件失败！:(', status: 200 },
-        })
+          meta: { msg: "上传文件失败！:(", status: 200 },
+        });
       } else {
-        const { filename, destination, mimetype } = req.file
-        let basePath = __dirname + `/../${destination}`
-        let fileType = mimetype.substr(mimetype.lastIndexOf('/') + 1)
-        fileReName(basePath + filename, basePath + filename + '.' + fileType)
-        req.body.photo = req.file.filename
-        next()
+        const { filename, destination, mimetype } = req.file;
+        let basePath = __dirname + `/../${destination}`;
+        let fileType = mimetype.substr(mimetype.lastIndexOf("/") + 1);
+        fileReName(basePath + filename, basePath + filename + "." + fileType);
+        req.body.photo = req.file.filename;
+        next();
       }
-    })
+    });
   },
   // 多文件上传
   uploads: (req, res, next) => {
-    let upload = multer({ dest: 'upload/' }).array('photo', 3)
-    upload(req, res, (err) => { })
+    let upload = multer({ dest: "upload/" }).array("photo", 3);
+    upload(req, res, (err) => {});
   },
-}
-module.exports = middlewares
+};
+module.exports = middlewares;
